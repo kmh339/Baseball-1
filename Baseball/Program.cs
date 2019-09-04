45994be1b93dcd395001962a8307c14984dd3dff
thkim <cplkimth@naver.com>
2019-09-04 15:54:59 +0900

    .

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -11,6 +11,28 @@ namespace Baseball
         static void Main(string[] args)
         {
             // 1 정답을 생성한다.
+            Random random = new Random();
+            int answer0;
+            int answer1;
+            int answer2;
+
+            while (true)
+            {
+                answer0 = random.Next(10);
+                answer1 = random.Next(10);
+                answer2 = random.Next(10);
+
+                if (answer0 != answer1 && answer1 != answer2 && answer2 != answer0)
+                    break;
+            }
+
+            Console.WriteLine("[정답]");
+            Console.Write(" " + answer0);
+            Console.Write(" " + answer1);
+            Console.Write(" " + answer2);
+            Console.WriteLine();
+
+
 
             // 2 추측을 입력받는다
 
